79c679cfdcea9ed7fa0ba890a8875d305699941c
egorryzhov71 <egorryzhov06@gmail.com>
2025-10-22 11:33:43 +0000

    sada

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /workspaces/C++
+# Build directory: /workspaces/C++/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(FiguresTests "/workspaces/C++/build/figures_tests")
+set_tests_properties(FiguresTests PROPERTIES  _BACKTRACE_TRIPLES "/workspaces/C++/CMakeLists.txt;30;add_test;/workspaces/C++/CMakeLists.txt;0;")
